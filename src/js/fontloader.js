1581376f94b579e60a9daa62126e54b6920d0ad7
Aleksandra Diachenko <aleksandra.diachenko@yalantis.com>
2017-01-11 13:53:35 +0200

    Change checking on woff2 support

--- src/js/fontloader.js
+++ src/js/fontloader.js
@@ -34,7 +34,7 @@ function loadFont(fontName, woffUrl, woff2Url, onlyLoadFontOnSecondPageload) {
     if (storedFontCss && (storedFontUrl === woffUrl || storedFontUrl === woff2Url)) {
         // the css is still in the localStorage
         // AND it was loaded from one of the current URLs
-        
+
         // 4. Applying the font style sheet
         styleElement.textContent = storedFontCss;
     } else {
@@ -51,7 +51,7 @@ function loadFont(fontName, woffUrl, woff2Url, onlyLoadFontOnSecondPageload) {
         request.open('GET', url);
         request.onload = function() {
             if (request.status >= 200 && request.status < 400) {
-                
+
                 // 7. Updating localStorage with the fresh data and applying the font data
                 loSto[localStorageUrlKey] = url;
                 loSto[localStorageCssKey] = request.responseText;
@@ -69,9 +69,8 @@ function loadFont(fontName, woffUrl, woff2Url, onlyLoadFontOnSecondPageload) {
             return false;
         }
 
-        var f = new FontFace('t', 'url("data:application/font-woff2,") format("woff2")', {});
-    	var p = f.load();
-    	try {p.then(null, function(){});}catch(e){}
-    	return f.status === 'loading';
+        var f = new FontFace('t', 'url("data:application/font-woff2;base64,d09GMgABAAAAAADcAAoAAAAAAggAAACWAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAABk4ALAoUNAE2AiQDCAsGAAQgBSAHIBtvAcieB3aD8wURQ+TZazbRE9HvF5vde4KCYGhiCgq/NKPF0i6UIsZynbP+Xi9Ng+XLbNlmNz/xIBBqq61FIQRJhC/+QA/08PJQJ3sK5TZFMlWzC/iK5GUN40psgqvxwBjBOg6JUSJ7ewyKE2AAaXZrfUB4v+hze37ugJ9d+DeYqiDwVgCawviwVFGnuttkLqIMGivmDg") format("woff2")', {});
+    	f.load()['catch'](function() {});
+    	return f.status == 'loading' || f.status == 'loaded';
     }
 }
